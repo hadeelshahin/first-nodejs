1b277ff1554cff2ff916f73a24a9bf94eedb0b7a
hadeelshahin <sha.99hadeel@gmail.com>
2024-02-27 04:24:32 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+//onsole.log("Hello from Nodejs");
+const fs = require("fs"); //file system object i imported
+fs.writeFileSync("hello.txt", "hello from NodeJs"); //this method will write a file to our HDD 
